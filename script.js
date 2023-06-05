9b73c8b9aa85c765b97b2fde8de62fa85f0d7f72
Giga_Dev148 <gigakatamadze0@gmail.com>
2023-06-05 13:26:56 +0400

    Building logic of calculator with the help of classes

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
-const cBtn = document.getElementById("cBtn");
-const divideBtn = document.getElementById("divideBtn");
-const multiplyBtn = document.getElementById("multiplyBtn");
-const removeBtn = document.getElementById("removeBtn");
-const seventBtn = document.getElementById("seventBtn");
-const eightBtn = document.getElementById("eightBtn");
-const nineBtn = document.getElementById("nineBtn");
-const minusBtn = document.getElementById("minusBtn");
-const fourBtn = document.getElementById("fourBtn");
-const fiveBtn = document.getElementById("fiveBtn");
-const sixBtn = document.getElementById("sixBtn");
-const plusBtn = document.getElementById("plusBtn");
-const oneBtn = document.getElementById("oneBtn");
-const twoBtn = document.getElementById("twoBtn");
-const threeBtn = document.getElementById("threeBtn");
-const equalBtn = document.getElementById("equalBtn");
-const percentBtn = document.getElementById("percentBtn");
-const zeroBtn = document.getElementById("zeroBtn");
-const dotBtn = document.getElementById("dotBtn");
+class Calculator {
+  constructor(previousOperaitonTextElement, currentOperationTextElement) {
+    this.previousOperaitonTextElement = previousOperaitonTextElement;
+    this.currentOperationTextElement = currentOperationTextElement;
+    this.clear();
+  }
+
+    clear() {
+      this.currentOperand = "";
+      this.previousOperand = "";
+      this.operation = undefined;
+    }
+
+  delete() {}
+
+  appendNumber(number) {
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+    console.log(this.currentOperand);
+  }
+
+  chooseOperation(operation) {}
+
+  compute() {}
+
+  updateDisplay() {
+    this.currentOperationTextElement.innerText = this.currentOperand;
+  }
+}
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const previousOperaitonTextElement = document.querySelector(
+  "[data-previous-operation]"
+);
+const currentOperationTextElement = document.querySelector(
+  "[data-current-operation]"
+);
+
+const calculator = new Calculator(
+  previousOperaitonTextElement,
+  currentOperationTextElement
+);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
